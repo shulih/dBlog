e80d8133ed9e9e7ef9cccb7c0c9c1f7c99b88557
shulih <shuli.hecht@gmail.com>
2024-02-27 22:09:16 +0200

    Blogs exercise creation

---
+++ ReactWs/dojo-blog/src/useFetch.js
@@ -0,0 +1,86 @@
+/*import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        if(err.name === 'AbortError'){
+          console.log('fetch aborted')
+                } else {
+                  setIsPending(false);
+                  setError(err.message + 'url =' + { url });
+                  
+                 
+                }
+        
+      })
+    }, 1000);
+
+
+    console.log('clean up');
+  }, [url])
+
+  return { data, isPending, error };
+}
+ 
+export default useFetch; */
+
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        }
+      })
+    }, 1000);
+
+    // abort the fetch
+    return () => abortCont.abort();
+  }, [url])
+
+  return { data, isPending, error };
+}
+ 
+export default useFetch;
